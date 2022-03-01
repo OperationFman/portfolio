e17653143e499087bac6d4eddc7ad0d225621842
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2022-03-02 10:11:10 +1100

    Add Navbar tabs

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -1,16 +1,41 @@
-import { Button } from "@mui/material";
+import { Button, IconButton, Tab, Tabs } from "@mui/material";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import Brightness6Icon from "@mui/icons-material/Brightness6";
+import Brightness3Icon from "@mui/icons-material/Brightness3";
 
 export const Navbar = (props: any) => {
   const { darkMode, setDarkMode } = props;
+  const [tab, setTab] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newTab: number) => {
+    setTab(newTab);
+  };
 
   return (
     <nav>
-      <Link href="/">Navbar</Link>
-      <Button variant="text" onClick={() => setDarkMode(!darkMode)}>
-        TOGGLE DARKMODE - Dont forget to add types to all this
-      </Button>
+      <Tabs
+        value={tab}
+        onChange={handleChange}
+        aria-label="icon label tabs example"
+        sx={{
+          color: "success.main",
+        }}
+      >
+        <Tab label="FRANKLIN V MOON" />
+        <Tab icon={<PersonOutlineIcon />} label="SKILLS" />
+        <Tab icon={<PersonOutlineIcon />} label="TUTORIALS" />
+        <Tab icon={<PersonOutlineIcon />} label="PROJECTS" />
+      </Tabs>
+
+      <IconButton size="large" onClick={() => setDarkMode(!darkMode)}>
+        {darkMode ? (
+          <Brightness3Icon fontSize="inherit" />
+        ) : (
+          <Brightness6Icon fontSize="inherit" />
+        )}
+      </IconButton>
     </nav>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,12 @@ import React, { useState } from "react";
 import { Navbar } from "../components/navigation/Navbar";
 import { GlobalTheme } from "../themes/GlobalTheme";
 
-import Brightness6Icon from "@mui/icons-material/Brightness6";
-import Brightness3Icon from "@mui/icons-material/Brightness3";
-
 function MyApp({ Component, pageProps }: AppProps) {
   const [darkMode, setDarkMode] = useState(false);
 
   return (
     <GlobalTheme darkMode={darkMode}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
-
-      <Brightness3Icon />
-      <Brightness6Icon />
 
       <Component {...pageProps} />
     </GlobalTheme>
