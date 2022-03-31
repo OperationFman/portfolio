413df6cd7f90b5d1f536d6af138fc258c4226dd2
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2022-03-31 18:27:10 +1100

    Add experimental styling for tutorial pages

--- pages/tutorials/[link].tsx
+++ pages/tutorials/[link].tsx
@@ -1,13 +1,57 @@
 import { InferGetServerSidePropsType } from "next";
+import dynamic from "next/dynamic";
 import { NotionAPI } from "notion-client";
 import { ExtendedRecordMap } from "notion-types";
 import { NotionRenderer } from "react-notion-x";
 import { getTutorialMetaDataByLink } from "../../src/tutorials/tutorialDataService";
 import { TutorialMetaData } from "../../src/tutorials/types";
 import { ErrorContent } from "../../utils/ErrorContent";
+import { isClientSide } from "../../utils/isClientSide";
+import { Code } from "react-notion-x/build/third-party/code";
+import { Collection } from "react-notion-x/build/third-party/collection";
+import { Container } from "@mui/material";
+import useDeviceDetect from "../../utils/useDeviceDetect";
 
 type ServerSideContext = {
   params: { link: string | string[] | undefined };
+};
+
+const PageContent = ({
+  notionPage,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const { isMobile } = useDeviceDetect();
+
+  // Todo: Create hook 'isDarkMode' as below is always dark when the page is first loaded
+  const darkMode = isClientSide()
+    ? localStorage.getItem("dark-mode") === "true"
+    : true;
+
+  if (!notionPage) {
+    return <ErrorContent />;
+  }
+
+  return (
+    <Container maxWidth={"md"} sx={{ overflow: "hidden" }}>
+      <div
+        style={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          transform: "translateY(-325px)",
+        }}
+      >
+        <NotionRenderer
+          recordMap={notionPage}
+          fullPage={true}
+          darkMode={darkMode}
+          previewImages={true}
+          components={{ Code, Collection }}
+        />
+      </div>
+    </Container>
+  );
 };
 
 const validateLinkAndFetchMetaData = (
@@ -19,40 +63,25 @@ const validateLinkAndFetchMetaData = (
   return getTutorialMetaDataByLink(link);
 };
 
-const PageContent = ({
-  notionPage,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  if (!notionPage) {
-    return <ErrorContent />;
-  }
-
-  return (
-    <NotionRenderer recordMap={notionPage} fullPage={true} darkMode={true} />
-  );
-};
-
 export const getServerSideProps = async (context: ServerSideContext) => {
   const notion = new NotionAPI();
   const metaData = validateLinkAndFetchMetaData(context.params.link);
+  const inValid = {
+    props: {
+      notionPage: false,
+    },
+  };
 
   if (!metaData) {
-    return {
-      props: {
-        notionPage: false,
-      },
-    };
+    return inValid;
   }
-  // TODO: Combine into validate and fetch to remove second 'false' response
+
   const notionPage: ExtendedRecordMap = await notion.getPage(
     metaData.notionPage
   );
 
   if (!notionPage) {
-    return {
-      props: {
-        notionPage: false,
-      },
-    };
+    return inValid;
   }
 
   return {
