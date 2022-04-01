aaf0315dbb63cc422b96b6578f20ecbf0e4c9514
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2022-04-01 12:34:50 +1100

    Add loading bar for tutorial cards

--- src/tutorials/components/cards/TutorialCard.tsx
+++ src/tutorials/components/cards/TutorialCard.tsx
@@ -7,8 +7,10 @@ import {
   Tooltip,
   Typography,
   IconButton,
+  LinearProgress,
 } from "@mui/material";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { TutorialMetaData } from "../../types";
 import { subTitleShortener, titleShortener } from "./textFormatter";
 
@@ -20,9 +22,13 @@ type TutorialCardProps = {
 export const TutorialCard = (props: TutorialCardProps): JSX.Element => {
   const { cardData, accentColor } = props;
   const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
   return (
     <IconButton
       onClick={() => {
+        setLoading(true);
         router.push(`tutorials/${cardData.link}`);
       }}
       disableRipple
@@ -57,19 +63,27 @@ export const TutorialCard = (props: TutorialCardProps): JSX.Element => {
                 {cardData.topic}
               </Typography>
             </Box>
-            <Typography gutterBottom variant="h5">
+            <Typography
+              gutterBottom
+              variant="h5"
+              sx={{ marginBottom: loading ? "0px" : "5px" }}
+            >
               {titleShortener(cardData.title)}
             </Typography>
             <Tooltip title={cardData.subTitle} followCursor enterDelay={200}>
               <Typography
                 variant="body2"
                 color="text.secondary"
-                sx={{ paddingLeft: "5px", fontWeight: 200 }}
+                sx={{
+                  paddingLeft: "5px",
+                  fontWeight: 200,
+                }}
               >
                 {subTitleShortener(cardData.subTitle)}
               </Typography>
             </Tooltip>
           </CardContent>
+          {loading && <LinearProgress color="secondary" />}
         </CardActionArea>
       </Card>
     </IconButton>
