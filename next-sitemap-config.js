79e16261785f8bcaee3e643c569e04636ca3ea92
Franklin Moon <franklin.m.moon@gmail.com>
2022-04-17 09:43:20 +1000

    Add robots.txt and sitemap.xml

---
+++ next-sitemap-config.js
@@ -0,0 +1,15 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: 'https://franklin-v-moon.dev',
+    generateRobotsTxt: true,
+    changefreq: 'daily',
+    robotsTxtOptions: {
+        policies: [
+          {
+            userAgent: '*',
+            allow: '/',
+          }
+        ],
+    },
+  }
