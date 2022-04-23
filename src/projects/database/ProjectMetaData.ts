112b393f5e311798a4fc625627a292dae79cbe85
Franklin Moon <franklin.m.moon@gmail.com>
2022-04-24 09:59:55 +1000

    Revert "[Refactor] Project page formatting"
    This reverts commit 36dd06afe2199f8093f791fc3b41b7dfb71d6049.

--- src/projects/database/ProjectMetaData.ts
+++ src/projects/database/ProjectMetaData.ts
@@ -2,21 +2,11 @@ import { ProjectMetaData } from '../types'
 
 export const projectMetaData: ProjectMetaData[] = [
     {
-        title: "Travel Blog",
-        link: "travel-blog",
-        notionPage: "efg456",
-        created: 2,
-        thumbnail: "/projects/test.jpeg",
-        subTitle: "My travels or something",
-        deployedURL: "http://opfman.pythonanywhere.com/",
-        repoURL: "https://github.com/OperationFman/Krank",
-    },
-    {
         title: "Krank",
         link: "krank",
         notionPage: "abcd123",
         created: 1,
-        thumbnail: "/projects/krank.png",
+        thumbnail: "/projects/test.jpeg",
         subTitle: "Food Roulette & Currency Conversion Tool",
         deployedURL: "http://opfman.pythonanywhere.com/",
         repoURL: "https://github.com/OperationFman/Krank",
