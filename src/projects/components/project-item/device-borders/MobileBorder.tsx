6468e650fa0e98a02a0d3e70b3f2de221ccece58
Franklin Moon <franklin.m.moon@gmail.com>
2022-04-27 13:46:26 +1000

    Add all remaining project content to database

--- src/projects/components/project-item/device-borders/MobileBorder.tsx
+++ src/projects/components/project-item/device-borders/MobileBorder.tsx
@@ -13,7 +13,7 @@ export const MobileBorder = (props: MobileBorderProps) => {
             position: "absolute",
             right: isMobile ? "0" : "8%",
             top: "22%",
-            height: "70%",
+            height: "65%",
             width: isMobile ? "18%" : "14%",
             border: isMobile ? "4px solid #282424" : "5px solid #282424",
             borderRadius: "15px",
