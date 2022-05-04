e026ce2facaea0a6a15461fe21d6a643b6c8c4a4
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2022-05-05 09:37:24 +1000

    [Test] Add react-spring parallax

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,57 @@
-import { Button } from "@mui/material";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useRef } from "react";
+import Image from "next/image";
+import { url } from "inspector";
+import moon from "../public/homepage/parallax/moon.png";
+import land from "../public/homepage/parallax/land.png";
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Franklin Von Moon</title>
-        <meta
-          name="Home"
-          content="This guy is Franklin V Moon and he's a Full Stack Software Developer. This site contains his Bio, Skills, Tutorials and Projects"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const ref = useRef();
 
-      <h1>Hello World</h1>
-    </div>
+  if (!ref) {
+    return <h1>memes</h1>;
+  }
+
+  return (
+    <>
+      <div>
+        <Parallax pages={6} ref={ref}>
+          <ParallaxLayer
+            offset={0}
+            speed={1}
+            factor={2}
+            style={{
+              backgroundImage: `url(${moon.src})`,
+              backgroundSize: "cover",
+            }}
+          />
+
+          <ParallaxLayer
+            offset={2}
+            speed={1}
+            factor={4}
+            style={{
+              backgroundImage: `url(${land.src})`,
+              backgroundSize: "cover",
+            }}
+          />
+
+          <ParallaxLayer offset={0.2} speed={0.05}>
+            <h2>Parallax Text 1</h2>
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={3} speed={2}>
+            <h2>Parallax Text 2</h2>
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={6} speed={1}>
+            <h1>Rest of page</h1>
+          </ParallaxLayer>
+        </Parallax>
+      </div>
+    </>
   );
 };
 
--- src/projects/components/project-item/ProjectItem.tsx
+++ src/projects/components/project-item/ProjectItem.tsx
@@ -1,95 +1,101 @@
-import { Typography } from '@mui/material';
-import Image from 'next/image';
+import { Typography } from "@mui/material";
+import Image from "next/image";
 import Carousel from "react-material-ui-carousel";
 import useDeviceDetect from "../../../../utils/useDeviceDetect";
 import { ProjectMetaData } from "../../types";
 import { LaptopBorder } from "./device-borders/LaptopBorder";
 import { MobileBorder } from "./device-borders/MobileBorder";
-import { ExternalLinkButtons } from './ExternalLinkButtons';
+import { ExternalLinkButtons } from "./ExternalLinkButtons";
 
 type ProjectItemProps = {
-    metaData: ProjectMetaData
-}
+  metaData: ProjectMetaData;
+};
 
 export const ProjectItem = (props: ProjectItemProps): JSX.Element => {
-    const { metaData } = props;
-    const { isMobile } = useDeviceDetect()
+  const { metaData } = props;
+  const { isMobile } = useDeviceDetect();
 
-    
-    return (
-            <div style={isMobile ? {
-                padding: "0px 0px 100px 0px",
-            } : 
-            { 
-                padding: "0px 100px 100px 100px",
+  return (
+    <div
+      style={
+        isMobile
+          ? {
+              padding: "0px 0px 100px 0px",
             }
-            }>
-                <h1 style={{
-                    width: "fit-content",
-                    padding: "0px 100px 5px 0px",
-                    borderBottom: "2px solid #f44336",
-                    margin: "5px"
-                }}>{metaData.title}</h1>
+          : {
+              padding: "0px 100px 100px 100px",
+            }
+      }
+    >
+      <h1
+        style={{
+          width: "fit-content",
+          padding: "0px 100px 5px 0px",
+          borderBottom: "2px solid #f44336",
+          margin: "5px",
+        }}
+      >
+        {metaData.title}
+      </h1>
 
-                <ExternalLinkButtons metaData={metaData}/>
+      <ExternalLinkButtons metaData={metaData} />
 
-                <Carousel 
-                autoPlay={false} 
-                navButtonsAlwaysVisible={isMobile ? false : true} 
-                indicators={false}
-                fullHeightHover={false} 
-                animation={"slide"}
-                duration={500}
-                navButtonsProps={{
-                    style: {
-                        backgroundColor: "transparent",
-                        color: "grey"
-                    }
-                }}>
-                {
-                    metaData.images.desktop.map((item, i) => 
-                        <>
-                            <LaptopBorder key={i}>
-                                <Image
-                                    src={item} 
-                                    width={1280} 
-                                    height={720} 
-                                    alt="Picture of the website"
-                                    style={{
-                                        borderRadius: "5px 5px 0px 0px"
-                                    }}
-                                />
-                            </LaptopBorder>
-                        
-                            {metaData.images.mobile ? 
-                                <MobileBorder>
-                                    <Image  
-                                        src={metaData.images.mobile[i]} 
-                                        layout="fill"
-                                        alt="Picture of the website on mobile"
-                                        style={{
-                                            borderRadius: "10px",
-                                            position: "absolute",
-                                            right: "50px",
-                                        }}>
-                                    </Image>
-                                </MobileBorder> 
-                            : null}
-                        </>
-                    )
-                }
-                </Carousel>
-                
-                <Typography
-                    variant="body2"
-                    color="text.secondary"
-                    sx={{
-                        textAlign: "center",
-                        marginTop: "30px"
-                    }}
-                >
-                    {metaData.subTitle}
-                </Typography>
-            </div>
-    )
-}
+      <Carousel
+        autoPlay={false}
+        navButtonsAlwaysVisible={isMobile ? false : true}
+        indicators={false}
+        fullHeightHover={false}
+        animation={"slide"}
+        duration={500}
+        navButtonsProps={{
+          style: {
+            backgroundColor: "transparent",
+            color: "grey",
+          },
+        }}
+      >
+        {metaData.images.desktop.map((item, i) => (
+          <>
+            <LaptopBorder key={i}>
+              <Image
+                src={item}
+                width={1280}
+                height={720}
+                alt="Picture of the website"
+                style={{
+                  borderRadius: "5px 5px 0px 0px",
+                }}
+              />
+            </LaptopBorder>
+
+            {metaData.images.mobile ? (
+              <MobileBorder>
+                <Image
+                  src={metaData.images.mobile[i]}
+                  layout="fill"
+                  alt="Picture of the website on mobile"
+                  style={{
+                    borderRadius: "10px",
+                    position: "absolute",
+                    right: "50px",
+                  }}
+                />
+              </MobileBorder>
+            ) : null}
+          </>
+        ))}
+      </Carousel>
+
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        sx={{
+          textAlign: "center",
+          marginTop: "30px",
+        }}
+      >
+        {metaData.subTitle}
+      </Typography>
+    </div>
+  );
+};
