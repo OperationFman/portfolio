1f6c91b157caddd5f9409a3cc2b5fd416bc45b6f
Franklin Moon <franklin.m.moon@gmail.com>
2022-06-24 22:17:36 +1000

    Add honeycomb design to Bio page

--- src/homepage/bio/Biography.tsx
+++ src/homepage/bio/Biography.tsx
@@ -1,20 +1,14 @@
 import { ParallaxLayer } from "@react-spring/parallax";
 
 export const Biography = () => {
-  return (
-    <ParallaxLayer
-      offset={1}
-      factor={1}
-      style={{
-        backgroundColor: "#01579B",
-      }}
-    >
-      <h1 style={{ paddingTop: "100px" }}>Name & Role(s) (Changing)</h1>
-      <ul>
-        <li>Quick bio</li>
-        <li>Who I am (+ mob)</li>
-        <li>Where I am</li>
-      </ul>
-    </ParallaxLayer>
-  );
+	return (
+		<ParallaxLayer offset={1} factor={1}>
+			<h1 style={{ paddingTop: "100px" }}>Name & Role(s) (Changing)</h1>
+			<ul>
+				<li>Quick bio</li>
+				<li>Who I am (+ mob)</li>
+				<li>Where I am</li>
+			</ul>
+		</ParallaxLayer>
+	);
 };
