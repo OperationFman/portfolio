d016085f1b0987ed9f960ca47d1af051b6601064
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2022-07-04 17:12:36 +1000

    [Refactor] Invert logic for shouldShowScroll

--- utils/shouldShowScrollbar.tsx
+++ utils/shouldShowScrollbar.tsx
@@ -2,17 +2,19 @@ import { NextRouter, useRouter } from "next/router";
 import { isClientSide } from "./isClientSide";
 
 export const shouldShowScrollBar = (router: NextRouter) => {
+  // React Parallax tool forces a second scrollbar to appear on the homepage
+  // This file removes the default scrollbar on the homepage but shows it for all other pages
   if (!isClientSide()) {
     return;
   }
 
   const handleRouteChange = (url: string) => {
-    if (url !== "/" && isClientSide()) {
-      document.body.style.overflow = "visible";
+    if (url === "/") {
+      document.body.style.overflow = "hidden";
       return;
     }
 
-    document.body.style.overflow = "hidden";
+    document.body.style.overflow = "visible";
   };
 
   router.events.on("routeChangeStart", handleRouteChange);
