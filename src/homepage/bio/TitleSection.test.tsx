ec69b5a0c2ca5de4c27dd78ea7eabb0a7dc37922
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2022-08-12 10:10:19 +1000

    Removed temporal test

--- src/homepage/bio/TitleSection.test.tsx
+++ src/homepage/bio/TitleSection.test.tsx
@@ -2,27 +2,6 @@ import { render } from "@testing-library/react";
 import { timeWorked, pluralTime, TitleSection } from "./TitleSection";
 
 describe("Title Section", () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-    jest.clearAllTimers;
-  });
-
-  it("displays correct temporal since career started", () => {
-    jest.useFakeTimers();
-    jest.setSystemTime(new Date("July 1, 2024 08:59:59"));
-
-    const result = timeWorked();
-
-    expect(result).toEqual({
-      years: 2,
-      months: 10,
-      days: 28,
-      hours: 23,
-      minutes: 59,
-      seconds: 59,
-    });
-  });
-
   describe("plural time", () => {
     it("adds and returns an s when unit of time when it is 1", () => {
       const result = pluralTime("year", 1);
