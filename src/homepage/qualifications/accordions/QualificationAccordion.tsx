2d43f359f610a8e2575fa375b5192ba618fd29a7
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2022-08-19 17:06:30 +1000

    Add Mantine components

--- src/homepage/qualifications/accordions/QualificationAccordion.tsx
+++ src/homepage/qualifications/accordions/QualificationAccordion.tsx
@@ -1,72 +1,69 @@
-import { Accordion, Typography } from "@mui/material";
-import { useState } from "react";
-import { PageContainer } from "../../../global/PageContainer";
-import { AccordionDetails, AccordionSummary } from "./AccordionUtil";
+import { Group, Avatar, Text, Accordion } from "@mantine/core";
+
+const charactersList = [
+  {
+    id: "bender",
+    image: "https://img.icons8.com/clouds/256/000000/futurama-bender.png",
+    label: "Bender Bending Rodríguez",
+    description: "Fascinated with cooking, though has no sense of taste",
+    content:
+      "Bender Bending Rodríguez, (born September 4, 2996), designated Bending Unit 22, and commonly known as Bender, is a bending unit created by a division of MomCorp in Tijuana, Mexico, and his serial number is 2716057. His mugshot id number is 01473. He is Fry's best friend.",
+  },
+
+  {
+    id: "carol",
+    image: "https://img.icons8.com/clouds/256/000000/futurama-mom.png",
+    label: "Carol Miller",
+    description: "One of the richest people on Earth",
+    content:
+      "Carol Miller (born January 30, 2880), better known as Mom, is the evil chief executive officer and shareholder of 99.7% of Momcorp, one of the largest industrial conglomerates in the universe and the source of most of Earth's robots. She is also one of the main antagonists of the Futurama series.",
+  },
+
+  {
+    id: "homer",
+    image: "https://img.icons8.com/clouds/256/000000/homer-simpson.png",
+    label: "Homer Simpson",
+    description: "Overweight, lazy, and often ignorant",
+    content:
+      "Homer Jay Simpson (born May 12) is the main protagonist and one of the five main characters of The Simpsons series(or show). He is the spouse of Marge Simpson and father of Bart, Lisa and Maggie Simpson.",
+  },
+];
+
+interface AccordionLabelProps {
+  label: string;
+  image: string;
+  description: string;
+}
+
+function AccordionLabel({ label, image, description }: AccordionLabelProps) {
+  return (
+    <Group noWrap>
+      <Avatar src={image} radius="xl" size="lg" />
+      <div>
+        <Text>{label}</Text>
+        <Text size="sm" color="dimmed" weight={400}>
+          {description}
+        </Text>
+      </div>
+    </Group>
+  );
+}
 
 export const QualificationAccordion = () => {
-  const [expanded, setExpanded] = useState<string | false>("panel1");
-
-  const handleChange =
-    (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
-      setExpanded(newExpanded ? panel : false);
-    };
+  const items = charactersList.map((item) => (
+    <Accordion.Item value={item.id} key={item.label}>
+      <Accordion.Control>
+        <AccordionLabel {...item} />
+      </Accordion.Control>
+      <Accordion.Panel>
+        <Text size="sm">{item.content}</Text>
+      </Accordion.Panel>
+    </Accordion.Item>
+  ));
 
   return (
-    <>
-      <Accordion
-        expanded={expanded === "panel1"}
-        onChange={handleChange("panel1")}
-      >
-        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-          <Typography>Masters of Information Systems and Finance</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-            lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion
-        expanded={expanded === "panel2"}
-        onChange={handleChange("panel2")}
-      >
-        <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-          <Typography>
-            Bachelors of Information Technology Professional Practice
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-            lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion
-        expanded={expanded === "panel3"}
-        onChange={handleChange("panel3")}
-      >
-        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
-          <Typography>
-            Certificate of Interactive Digital Communication and Design
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-            lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    </>
+    <Accordion chevronPosition="right" variant="contained">
+      {items}
+    </Accordion>
   );
 };
