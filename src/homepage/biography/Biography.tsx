dfcb585dfc04453543dcb6daba526703a88e1d1d
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2022-10-28 12:33:10 +1100

    :sparkles: Add programatic updating of ForYouCard

--- src/homepage/biography/Biography.tsx
+++ src/homepage/biography/Biography.tsx
@@ -41,16 +41,47 @@ export const Biography = () => {
 
 			<Grid container spacing={5} justifyContent='center'>
 				<Grid item>
-					<ForYouCard />
+					<ForYouCard
+						logo={"code"}
+						title={"Programming"}
+						paragraph1={
+							"Champion best practices like TDD, SOLID principles, Object Oriented design and Pair Programming."
+						}
+						paragraph2={"See Skills for more."}
+					/>
 				</Grid>
 				<Grid item>
-					<ForYouCard />
+					<ForYouCard
+						logo={"code"}
+						title={"Design"}
+						paragraph1={"Crafting user interfaces, wireframes, and prototypes."}
+						paragraph2={
+							"Researching user needs and following design principles."
+						}
+					/>
 				</Grid>
 				<Grid item>
-					<ForYouCard />
+					<ForYouCard
+						logo={"code"}
+						title={"Post-Production"}
+						paragraph1={
+							"Producing, editing, color correcting, and rendering motion pictures."
+						}
+						paragraph2={
+							"Skills include VFX, animation and game engine production."
+						}
+						paragraphGap={false}
+					/>
 				</Grid>
 				<Grid item>
-					<ForYouCard />
+					<ForYouCard
+						logo={"code"}
+						title={"Volunteering"}
+						paragraph1={
+							"Sharing knowledge and resources with those eager to learn but who do not have the means to pursue careers in technology. This is my life pursuit."
+						}
+						paragraph2={"See Skills for more."}
+					/>
 				</Grid>
 			</Grid>
 
--- src/homepage/biography/components/ForYouCard.tsx
+++ src/homepage/biography/components/ForYouCard.tsx
@@ -1,7 +1,27 @@
 import { Card, CardContent, Typography } from "@mui/material";
 import Image from "next/future/image";
+import ShowIf from "../../../../utils/ShowIf";
 
-export const ForYouCard = () => {
+export const ForYouCard = ({
+	logo,
+	title,
+	paragraph1,
+	paragraph2,
+	paragraphGap = true,
+}: {
+	logo: string;
+	title: string;
+	paragraph1: string;
+	paragraph2?: string;
+	paragraphGap?: boolean;
+}) => {
+	const titleOnlyFirstLetter = title
+		.split(" ")
+		.map((word) => word[0])
+		.join("");
+
+	const titleNoFirstLetter = title.substring(1);
+
 	return (
 		<Card sx={{ width: 415, height: 440 }}>
 			<CardContent
@@ -14,8 +34,8 @@ export const ForYouCard = () => {
 					padding: "30px",
 				}}>
 				<Image
-					src={`/homepage/biography/code.svg`}
-					alt='Train'
+					src={`/homepage/biography/${logo}.svg`}
+					alt={logo}
 					width={60}
 					height={60}
 					style={{ width: "100%", marginBottom: "20px" }}
@@ -25,21 +45,29 @@ export const ForYouCard = () => {
 					variant='h5'
 					align='center'
 					style={{ width: "100%", fontWeight: "bold", marginBottom: "20px" }}>
-					<span style={{ color: "#1565C0" }}>P</span>
-					<span>rogramming</span>
+					<span style={{ color: "#1565C0" }}>{titleOnlyFirstLetter}</span>
+					<span>{titleNoFirstLetter}</span>
 				</Typography>
 
 				<Typography
 					align='center'
 					variant='h5'
-					style={{ width: "100%", marginBottom: "40px", lineHeight: "40px" }}>
-					Champion best practices like TDD, SOLID principles, Object Oriented
-					design and Pair Programming.
+					style={{
+						width: "100%",
+						marginBottom: paragraphGap ? "40px" : "",
+						lineHeight: "40px",
+					}}>
+					{paragraph1}
 				</Typography>
 
-				<Typography align='center' variant='h5' style={{ width: "100%" }}>
-					See Skills for more.
-				</Typography>
+				<ShowIf condition={paragraph2}>
+					<Typography
+						align='center'
+						variant='h5'
+						style={{ width: "100%", lineHeight: "40px" }}>
+						{paragraph2}
+					</Typography>
+				</ShowIf>
 			</CardContent>
 		</Card>
 	);
