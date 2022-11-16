2037cc004855fc3f1a5cb7cb80144328221ff520
Franklin Moon <franklin.m.moon@gmail.com>
2022-11-16 14:31:11 +1100

    :lipstick: Replace headshot placeholder with illustrated avatar

--- src/homepage/biography/Biography.tsx
+++ src/homepage/biography/Biography.tsx
@@ -1,10 +1,15 @@
 import Image from "next/future/image";
+import { useContext } from "react";
+import { DarkMode } from "../../../themes/GlobalTheme";
 import ShowIf from "../../../utils/ShowIf";
 import useDeviceDetect from "../../../utils/useDeviceDetect";
 import { BioDescription } from "./BioDescription";
 
 export const Biography = () => {
 	const { isMobile } = useDeviceDetect();
+	const darkMode = useContext(DarkMode);
+
+	const selectedTheme = darkMode ? "dark" : "light";
 
 	return (
 		<div
@@ -17,7 +22,7 @@ export const Biography = () => {
 			<ShowIf condition={!isMobile}>
 				<div style={{ alignItems: "center" }}>
 					<Image
-						src='/homepage/placeholder.png'
+						src={`/homepage/biography/headshot-${selectedTheme}.svg`}
 						width='480'
 						height='300'
 						alt='Head shot'
