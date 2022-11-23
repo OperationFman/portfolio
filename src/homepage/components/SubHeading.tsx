eef60cb588d4aacfc8837502d4598207d76f4d90
Franklin Moon <42459707+OperationFman@users.noreply.github.com>
2022-11-23 14:36:17 +1100

    :fire: Remove Typography from SubHeading to fix not rendering correctly on deployed environment

--- src/homepage/components/SubHeading.tsx
+++ src/homepage/components/SubHeading.tsx
@@ -3,13 +3,13 @@ import React from "react";
 
 export const SubHeading = ({ text }: { text: string }) => {
 	return (
-		<div className='flex justify-center mt-[150px] mb-[50px] '>
-			<Typography variant='h3' align='center' className='font-bold'>
+		<div className='flex justify-center mt-[150px] mb-[50px]'>
+			<h1 className='text-center text-5xl font-bold'>
 				{text}
 				<div className='flex relative justify-center'>
-					<div className='absolute w-[150px] t-[2px] border-solid border-b-[5px] border-[#42a5f5] opacity-20  md:inset-[-10%] md:w-[120%]'></div>
+					<div className='absolute w-[150px] mt-[6px] border-solid border-b-[5px] border-[#42a5f5] opacity-20  md:inset-[-10%] md:w-[120%]'></div>
 				</div>
-			</Typography>
+			</h1>
 		</div>
 	);
 };
