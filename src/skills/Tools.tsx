bc487b655a65a18361dea65f886d60d1fd0d1687
OperationFman <franklin.m.moon@gmail.com>
2022-12-07 16:03:36 +1100

    :recycle: Remove MUI Typography from tools skills section

--- src/skills/Tools.tsx
+++ src/skills/Tools.tsx
@@ -1,7 +1,7 @@
-import { SkillSubHeading } from "./components/SkillSubHeading";
 import ConstructionIcon from "@mui/icons-material/Construction";
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent } from "@mui/material";
 import { groupedToolsMetaData } from "../datasources/SkillsMetaData";
+import { SkillSubHeading } from "./components/SkillSubHeading";
 
 export const Tools = () => {
 	return (
@@ -10,23 +10,21 @@ export const Tools = () => {
 				<ConstructionIcon color='success' className='text-5xl' />
 			</SkillSubHeading>
 			<Card>
-				<CardContent className='mb-5 pl-10 flex flex-col flex-wrap lg:pl-12 lg:h-[500px]'>
+				<CardContent className='mb-5 pl-10 flex flex-col flex-wrap lg:pl-12 lg:h-[520px]'>
 					{groupedToolsMetaData.map((item) => {
 						return (
 							<div
 								key={`${item.grouping} tools`}
 								className='w-[94%] mr-2 lg:w-[220px]'>
-								<h1 className='sm:mb-2'> {item.grouping} </h1>
-								<div className='flex flex-row flex-wrap'>
+								<h1 className='mb-[-7px]'> {item.grouping} </h1>
+								<div className='flex flex-row flex-wrap mb-4'>
 									{item.tools.map((tool) => {
 										return (
-											<Typography
-												variant='body1'
-												color='text.secondary'
+											<p
 												key={tool}
-												className='w-[50%] pr-10 mb-2 lg:pr-5 lg:mb-2'>
+												className='w-[50%] pr-10 mb-[-8px] lg:pr-5 '>
 												{tool}
-											</Typography>
+											</p>
 										);
 									})}
 								</div>
