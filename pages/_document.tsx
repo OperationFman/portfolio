5312156aa766cd79076ae403a21656149803ea31
OperationFman <franklin.m.moon@gmail.com>
2022-12-10 11:41:43 +1100

    :lipstick: Update font from Oswald to Monsterrat

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,44 +3,43 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 import React from "react";
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html lang='en'>
+				<Head>
+					<link
+						href='https://fonts.cdnfonts.com/css/montserrat'
+						rel='stylesheet'
+					/>
+					<style>
+						@import url('https://fonts.cdnfonts.com/css/montserrat');
+					</style>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
 MyDocument.getInitialProps = async (ctx) => {
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
+	const sheets = new ServerStyleSheets();
+	const originalRenderPage = ctx.renderPage;
 
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    });
+	ctx.renderPage = () =>
+		originalRenderPage({
+			enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+		});
 
-  const initialProps = await Document.getInitialProps(ctx);
+	const initialProps = await Document.getInitialProps(ctx);
 
-  return {
-    ...initialProps,
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
-    ],
-  };
+	return {
+		...initialProps,
+		styles: [
+			...React.Children.toArray(initialProps.styles),
+			sheets.getStyleElement(),
+		],
+	};
 };
