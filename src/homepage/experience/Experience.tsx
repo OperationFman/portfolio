68bf1a475307629b96650d16799a79d0d7b4418b
OperationFman <franklin.m.moon@gmail.com>
2023-01-11 15:25:49 +1100

    :recycle: Remove console log

--- src/homepage/experience/Experience.tsx
+++ src/homepage/experience/Experience.tsx
@@ -34,7 +34,6 @@ export const Experience = () => {
 			window.getComputedStyle(rightColumn).getPropertyValue("height"),
 		);
 
-		console.log({ smallerColumnHeight });
 		setStickyColumnLock(smallerColumnHeight);
 	}, []);
 
