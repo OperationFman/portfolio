e9bdd21652fb3dd542b3f18f62c1e76a957e1fe2
OperationFman <franklin.m.moon@gmail.com>
2023-01-25 13:05:39 +1100

    :recycle: Make homepage underlines thinner

--- src/homepage/components/SubHeading.tsx
+++ src/homepage/components/SubHeading.tsx
@@ -6,7 +6,7 @@ export const SubHeading = ({ text }: { text: string }) => {
 			<h1 className='text-center text-5xl font-bold'>
 				{text}
 				<div className='flex relative justify-center'>
-					<div className='absolute w-[150px] mt-[6px] border-solid border-b-[5px] border-[#42a5f5] opacity-20  md:inset-[-10%] md:w-[120%]'></div>
+					<div className='absolute w-[150px] mt-[6px] border-solid border-b-[1px] border-[#42a5f5] opacity-20  md:inset-[-10%] md:w-[120%]'></div>
 				</div>
 			</h1>
 		</div>
