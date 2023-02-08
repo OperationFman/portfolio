77e685dbbba853402af525fc86b70ea965df3137
OperationFman <franklin.m.moon@gmail.com>
2023-02-08 13:54:17 +1100

    :recycle: Reword all text across the site to be more professional and reduced typos

--- src/datasources/ProjectMetaData.ts
+++ src/datasources/ProjectMetaData.ts
@@ -12,7 +12,7 @@ export const projectMetaData: ProjectMetaData[] = [
 		],
 
 		subTitle:
-			"The site you're looking at right now. The goal was to create a beautiful front end application that I could use to sell myself and my skills but also be a repository of my knowledge to share freely with the world. Built using Typescript and the NEXTjs library with full TDD and deployed to Vercel through a full CI/CD pipeline using GitHub actions. It also has a focus on search engine optimization and uses Notion pages as a Content Management System.",
+			"The website you are currently viewing was designed with the objective of serving as a professional portfolio as well as a resource center for the dissemination of my knowledge to the public. The development of the website was carried out using Typescript and the NEXT.js library, incorporating a Test-Driven Development approach and deployed on Vercel via a continuous integration and continuous deployment pipeline facilitated by GitHub Actions. In addition, the website is optimized for search engines, and its content management system is based on Notion pages.",
 		deployedURL: "https://www.franklin-v-moon.dev/",
 		repoURL: "https://github.com/OperationFman/portfolio",
 	},
@@ -26,7 +26,7 @@ export const projectMetaData: ProjectMetaData[] = [
 		],
 
 		subTitle:
-			"Visually simple single page application to track a single stock and display the value of your holdings in USD and AUD, ideal if your workplace offers equity. Built using Typescript with the React library and a focus on Terraform to automatically provision and deploy a tested application on an AWS EC2 instance, using a Docker fed into AWS ECR.",
+			"A visually streamlined single-page application designed for tracking a single stock and displaying the value of one's holdings in both USD and AUD. This application is particularly suitable for individuals working in a workplace that offers equity. The application is built using Typescript with the React library, and is deployed using Terraform on an AWS EC2 instance. The deployment process utilizes Docker fed into AWS ECR to ensure a thoroughly tested and automatically provisioned application.",
 		repoURL: "https://github.com/OperationFman/Stock-Ticker-Overview",
 	},
 	{
@@ -42,7 +42,7 @@ export const projectMetaData: ProjectMetaData[] = [
 		],
 
 		subTitle:
-			"I simple management app that helps players select crew members to take on missions in the game series 'Mass Effect'. Users add and remove crew throughout the games and this tool shows you who you haven't brought on mission recently and randomly recommends them. Additionally, users can use Oauth to have their own account, saved across devices. Built using Javascript with the React Library and hosted on Heroku with Docker and a full 'deploy-to-prod-on-green' CI/CD pipeline in CircleCI.",
+			"A professional management application has been designed to assist players in selecting crew members for missions within the Mass Effect game series. The application enables players to add and remove crew members across various games, while providing insight into crew members who have not been utilized recently. The application also includes a feature that randomly recommends crew members for mission selection. Utilizing OAuth, players can access their account from multiple devices with ease, as the account is saved across devices. The application has been developed using Javascript and the React Library, and is hosted on Heroku, utilizing Docker, and maintained with a full CI/CD pipeline in CircleCI, using a 'deploy-to-prod-on-green' approach.",
 		repoURL: "https://github.com/OperationFman/Mass-Effect-Crewed",
 		uxURL:
 			"https://www.figma.com/proto/XL7Gz06sz0iLn4hyVpGnRz/Mass-Effect-Crewed?node-id=0%3A3&scaling=scale-down&page-id=0%3A1&starting-point-node-id=0%3A3",
@@ -61,7 +61,7 @@ export const projectMetaData: ProjectMetaData[] = [
 		],
 
 		subTitle:
-			"Investment tracking tools suffer from one of three problems, they're too complex, too expensive or too useless to be used regularly. Glance is a tool that lets you easily keep an overview of your investments, dividends and performance across services without being overwhelming for the average user. The goal is to create an experience as easy to use as Microsoft Excel with the design of a modern website. The MVP UX was completed and it is planned to be built using React/NEXTjs with a separate BFF using C#.",
+			"Investment tracking tools often struggle with one of three challenges: they are overly complicated, exorbitant in cost, or insufficient in functionality to warrant consistent utilization. Glance aims to provide a solution by offering a user-friendly platform for monitoring investments, dividends, and performance across various services, designed specifically for the average user. The aim is to create a seamless experience that combines the ease of use of Microsoft Excel with a sleek, modern web design. The minimum viable product (MVP) user experience has been completed and the development process will utilize React/NEXTjs as the primary framework, with a separate back-end service utilizing C#.",
 		uxURL:
 			"https://www.figma.com/proto/yfATxB2h0pjKwe1rdItWCC/Glance-Draft?node-id=0%3A3&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=0%3A3",
 	},
@@ -77,7 +77,7 @@ export const projectMetaData: ProjectMetaData[] = [
 		],
 
 		subTitle:
-			"Traveling the world is an important goal of mine, as I believe living, working and volunteering in other cultures is what can make you more open minded and receptive to new ideas. I built this site using Python with the Flask framework and Javascript with jquery for animations and automatic scaling. The focus is on design and presentation and uses external services like Google Drive for free video hosting and Mapbox for an interactive 3D map containing images.",
+			"One of my paramount objectives is to traverse the globe, as I am of the belief that exposure to diverse cultures through living, working, and volunteering can foster greater open-mindedness and receptiveness to novel concepts. To achieve this, I developed this website utilizing Python with the Flask framework and Javascript with jquery for dynamic animations and adaptive scaling. The emphasis has been placed on the design and visual presentation of the site, utilizing external resources such as Google Drive for video hosting and Mapbox for an immersive 3D map with embedded images.",
 		deployedURL: "http://frankstravelblog.pythonanywhere.com/",
 		repoURL: "https://github.com/OperationFman/FranksTravelBlog",
 	},
@@ -92,7 +92,7 @@ export const projectMetaData: ProjectMetaData[] = [
 		],
 
 		subTitle:
-			"My first ever programming project. A tool to help my partner and I while we were stuck in Japan during the 2020 pandemic. The webapp is primarily designed for mobile and has a 'food roulette' mode to store a list of meals we like and randomly suggests one when tapping 'generate'. \n It also has a simple currency conversion tool that quickly shows purchasing power of other fx compared to AUD. \n It was built using Python with the Flask framework and deployed to PythonAnywhere.",
+			"As a novice programmer, I embarked on my first programming project, which aimed to provide assistance to my partner and I during the COVID-19 pandemic in Japan. The web application, designed primarily for mobile devices, features a 'food roulette' mode, which allows us to store a list of our preferred meals and generates a random suggestion upon tapping the 'generate' button. Additionally, the app includes a straightforward currency conversion tool, which provides an instant comparison of purchasing power in different currencies relative to the Australian dollar. The application was constructed using Python and the Flask framework, and was hosted on PythonAnywhere.",
 		repoURL: "https://github.com/OperationFman/Krank",
 	},
 ];
