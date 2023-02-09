7d7610c5b32378b206abd8fd8177eb192cd2d7cd
OperationFman <franklin.m.moon@gmail.com>
2023-02-09 18:00:08 +1100

    :recycle: Rework the sun and moon so they animate in and out when darkmode is changed

--- src/homepage/parallax-art/ParallaxArt.tsx
+++ src/homepage/parallax-art/ParallaxArt.tsx
@@ -8,8 +8,8 @@ import styles from "./ParallaxArt.module.scss";
 export const ParallaxArt = () => {
 	const darkMode = useContext(DarkMode);
 	const [offSetY, setOffSetY] = useState(0);
-	
-	const theme = darkMode ? 'dark' : 'light'
+
+	const theme = darkMode ? "dark" : "light";
 
 	const handleScroll = () => {
 		// Stop rendering once parallax is off screen
@@ -31,6 +31,59 @@ export const ParallaxArt = () => {
 		};
 	}, []);
 
+	const [initialLoad, setInitialLoad] = useState(true);
+
+	const [sunRising, setSunRising] = useState(false);
+	const [sunAnchored, setSunAnchored] = useState(false);
+	const [sunSetting, setSunSetting] = useState(false);
+	const [sunInvisible, setSunInvisible] = useState(darkMode);
+
+	const [moonRising, setMoonRising] = useState(false);
+	const [moonAnchored, setMoonAnchored] = useState(false);
+	const [moonSetting, setMoonSetting] = useState(false);
+	const [moonInvisible, setMoonInvisible] = useState(!darkMode);
+
+	const transitionMoonUp = () => {
+		setMoonInvisible(false);
+		setMoonRising(true);
+
+		setSunAnchored(false);
+		setSunSetting(true);
+
+		setTimeout(() => {
+			setMoonRising(false);
+			setMoonAnchored(true);
+
+			setSunSetting(false);
+			setSunInvisible(true);
+		}, 1000);
+	};
+
+	const transitionSunUp = () => {
+		setMoonAnchored(false);
+		setMoonSetting(true);
+
+		setSunInvisible(false);
+		setSunRising(true);
+
+		setTimeout(() => {
+			setMoonSetting(false);
+			setMoonInvisible(true);
+
+			setSunRising(false);
+			setSunAnchored(true);
+		}, 1000);
+	};
+
+	useEffect(() => {
+		if (initialLoad) {
+			setInitialLoad(false);
+			return;
+		}
+
+		darkMode ? transitionMoonUp() : transitionSunUp();
+	}, [darkMode]);
+
 	return (
 		<ScrollAnimation animateIn='fadeIn' animateOnce delay={100}>
 			<div className={styles.outerContainer}>
@@ -38,30 +91,35 @@ export const ParallaxArt = () => {
 					<div className={setDark(styles, "background")} />
 
 					<Image
-						src={`/homepage/parallax/light/15_moon_sun-01.svg`}
+						src={"/homepage/parallax/light/15_moon_sun-01.svg"}
 						alt='Sun'
 						width='0'
 						height='0'
-						className={styles.layer}
-						 style={{
+						className={`${styles.layer} ${sunRising && styles.sunUp} ${
+							sunSetting && styles.sunDown
+						}`}
+						style={{
 							zIndex: -8,
-							transform: `translateY(${offSetY * 0.6}px)`
+							display: sunInvisible ? "none" : "inherit",
+							transform: sunAnchored ? `translateY(${offSetY * 0.6}px)` : "",
 						}}
 					/>
 					<Image
-						src={`/homepage/parallax/dark/15_moon_sun-01.svg`}
+						src={"/homepage/parallax/dark/15_moon_sun-01.svg"}
 						alt='Moon'
 						width='0'
 						height='0'
-						className={styles.layer}
+						className={`${styles.layer} ${moonRising && styles.moonUp} ${
+							moonSetting && styles.moonDown
+						}`}
 						style={{
 							zIndex: -8,
-							transform: `translateY(${offSetY * 0.6}px)`
+							display: moonInvisible ? "none" : "inherit",
+							transform: moonAnchored ? `translateY(${offSetY * 0.6}px)` : "",
 						}}
 					/>
-					{darkMode && 
+					{darkMode && (
 						<>
-							
 							<Image
 								src={`/homepage/parallax/${theme}/14_stars-01.svg`}
 								alt='Minor amount of stars'
@@ -106,7 +164,7 @@ export const ParallaxArt = () => {
 								}}
 							/>
 						</>
-					}
+					)}
 					<Image
 						src={`/homepage/parallax/${theme}/10_far_mountain-01.svg`}
 						alt='Farthest Mountain'
@@ -134,7 +192,9 @@ export const ParallaxArt = () => {
 						alt='Far clouds'
 						width='0'
 						height='0'
-						className={`${styles.layer} ${pageTop &&  styles.cloudLeftRightSlow}`}
+						className={`${styles.layer} ${
+							pageTop && styles.cloudLeftRightSlow
+						}`}
 						style={{
 							zIndex: -6,
 							transform: `translateY(${offSetY * 0.45}px)`,
@@ -167,7 +227,9 @@ export const ParallaxArt = () => {
 						alt='Close low clouds'
 						width='0'
 						height='0'
-						className={`${styles.layer} ${pageTop &&  styles.cloudLeftRightFast}`}
+						className={`${styles.layer} ${
+							pageTop && styles.cloudLeftRightFast
+						}`}
 						style={{
 							zIndex: -3,
 							transform: `translateY(${offSetY * 0.25}px)`,
