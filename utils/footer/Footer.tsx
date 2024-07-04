bb0f04a2bfab33e93e7cebeb3ff94fa33b88e1e3
Franklin Von Moon <franklin.m.moon@gmail.com>
2024-07-04 16:59:29 +0700

    :recycle: Redesign footer and add travel tab

--- utils/footer/Footer.tsx
+++ utils/footer/Footer.tsx
@@ -2,65 +2,50 @@ import { Button, Container } from "@mui/material";
 import { useRouter } from "next/router";
 import styles from "./Footer.module.scss";
 
+
 export const Footer = () => {
 	const toPageTop = () => {
 		window.scrollTo({ top: 0, behavior: "smooth" });
 	};
 
 	const router = useRouter();
+	const brightGrey = "brightGrey";
+
+	const links = [
+		{ label: "Home", path: "/" },
+		{ label: "Skills", path: "/skills" },
+		{ label: "Tutorials", path: "/tutorials" },
+		{ label: "Projects", path: "/projects" },
+		{ label: "Travel", path: "/travel" },
+	];
 
 	return (
-		<footer className={styles.container}>
-			<Button
-				onClick={() => toPageTop()}
-				variant='text'
-				color='brightGrey'
-				className={styles.button}>
-				Back To Top
-			</Button>
-			<Container maxWidth={"md"}>
-				<div className={styles.sitemap}>
-					<span>
-						<Button
-							color='brightGrey'
-							onClick={() => {
-								router.replace("/");
-							}}>
-							Homepage
-						</Button>
-					</span>
-					<span className={styles.divider}> | </span>
-					<span>
-						<Button
-							color='brightGrey'
-							onClick={() => {
-								router.replace("/skills");
-							}}>
-							Skills
-						</Button>
-					</span>
-					<span className={styles.divider}> | </span>
-					<span>
-						<Button
-							color='brightGrey'
-							onClick={() => {
-								router.replace("/tutorials");
-							}}>
-							Tutorials
-						</Button>
-					</span>
-					<span className={styles.divider}> | </span>
-					<span>
-						<Button
-							color='brightGrey'
-							onClick={() => {
-								router.replace("/projects");
-							}}>
-							Projects
-						</Button>
-					</span>
-				</div>
-			</Container>
-		</footer>
+		<>
+			<div className={styles.buttonContainer}>
+				<Button
+					onClick={toPageTop}
+					variant='text'
+					color={brightGrey}
+					className={styles.button}>
+					Back To Top
+				</Button>
+			</div>
+
+			<footer className={styles.bottomNav}>
+				<Container maxWidth={"md"}>
+					<div className={styles.sitemap}>
+						{links.map((link, index) => (
+							<span key={index}>
+								<Button
+									color={brightGrey}
+									onClick={() => router.replace(link.path)}>
+									{link.label}
+								</Button>
+							</span>
+						))}
+					</div>
+				</Container>
+			</footer>
+		</>
 	);
 };
