1148e4e06f7bb2d5e75794400cd23742babcae0e
Franklin Von Moon <franklin.m.moon@gmail.com>
2024-07-06 22:44:27 +0700

    :recycle: Fix scaling for mobile

--- pages/travel/[link].tsx
+++ pages/travel/[link].tsx
@@ -57,7 +57,7 @@ const VideoContent = ({
 									return (
 										<Grid item key={reel} className={styles.embeddedPost}>
 											<div className={styles.reelWrapper}>
-												<InstagramEmbed url={reel} width={415} />
+												<InstagramEmbed url={reel} width={380} />
 											</div>
 										</Grid>
 									);
@@ -67,7 +67,7 @@ const VideoContent = ({
 								instagramLinks.map((link) => {
 									return (
 										<Grid item key={link} className={styles.embeddedPost}>
-											<InstagramEmbed url={link} width={494} />
+											<InstagramEmbed url={link} width={450} />
 										</Grid>
 									);
 								})}
