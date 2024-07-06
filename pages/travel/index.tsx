27bed7a8d0e31c5787566f9edb9bf949dcaa32ae
Franklin Von Moon <franklin.m.moon@gmail.com>
2024-07-06 13:54:12 +0700

    :sparkles: Add grouping by year for travels page

--- pages/travel/index.tsx
+++ pages/travel/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/future/image";
 import { PageContainer } from "../../src/global/PageContainer";
 import { Footer } from "../../utils/footer/Footer";
 
@@ -7,12 +8,13 @@ import styles from "../../src/travel/index.module.scss";
 import { Grid } from "@mui/material";
 import { travelVideoMetaData } from "../../src/datasources/TravelMetaData";
 import ScrollAnimation from "react-animate-on-scroll";
+import { groupVideosByYear } from "../../src/travel/travelDataService";
 
 const Travel: NextPage = () => {
 	const description =
 		"Travel related content including completion map and travel videos, some public and some private of my experiences.";
 
-	const metaData = travelVideoMetaData;
+	const metaDataGroupedByYear = groupVideosByYear(travelVideoMetaData);
 
 	return (
 		<div>
@@ -35,21 +37,38 @@ const Travel: NextPage = () => {
 			</Head>
 
 			<PageContainer>
-				<Grid container className={styles.gridContainer}>
-					{metaData.map((dataItem, index) => {
+				{metaDataGroupedByYear.map((metaData, groupIndex) => {
+					{
 						return (
-							<ScrollAnimation
-								animateIn='fadeIn'
-								animateOnce
-								key={index}
-								delay={index * 50}>
-								<Grid item>
-									<h1>{dataItem.title}</h1>
+							<>
+								<h2 className={styles.yearHeading}>{metaData[0].year}</h2>
+								<Grid container className={styles.gridContainer}>
+									{metaData.map((dataItem, videoIndex) => {
+										return (
+											<ScrollAnimation
+												animateIn='fadeIn'
+												animateOnce
+												key={videoIndex}
+												delay={(groupIndex + videoIndex) * 50}>
+												<Grid item>
+													<div className={styles.videoCardContainer}>
+														<Image
+															src={`/travel/posters/${dataItem.slug}.png`}
+															alt={`${dataItem.title} poster`}
+															height='300'
+															width='200'
+															className={styles.videoCardImage}
+														/>
+													</div>
+												</Grid>
+											</ScrollAnimation>
+										);
+									})}
 								</Grid>
-							</ScrollAnimation>
+							</>
 						);
-					})}
-				</Grid>
+					}
+				})}
 			</PageContainer>
 			<Footer />
 		</div>
--- src/travel/travelDataService.ts
+++ src/travel/travelDataService.ts
@@ -7,3 +7,20 @@ export const getTravelMetaDataIndex = (
 ): number => {
 	return metaData.findIndex((item) => item.link === link);
 };
+
+export const groupVideosByYear = (
+	metadata: TravelVideoMetaData[],
+): TravelVideoMetaData[][] => {
+	const groupedVideos: { [year: number]: TravelVideoMetaData[] } = {};
+
+	for (const video of metadata) {
+		const year = video.year;
+
+		if (!groupedVideos[year]) {
+			groupedVideos[year] = [];
+		}
+		groupedVideos[year].push(video);
+	}
+
+	return Object.values(groupedVideos);
+};
