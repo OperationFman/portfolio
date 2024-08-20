089cea6f8fe57a17e553f61cf1b1c5920892f0ed
Franklin Von Moon <franklin.m.moon@gmail.com>
2024-08-20 17:47:21 +0700

    :recycle: Move load fade-in to page container

--- src/global/PageContainer.tsx
+++ src/global/PageContainer.tsx
@@ -4,8 +4,14 @@ import styles from "./PageContainer.module.scss";
 
 export const PageContainer = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<Container maxWidth='lg' className={styles.container}>
-			{children}
-		</Container>
+		<div
+			style={{
+				animation: "fadeIn 1000ms ease-in-out",
+				opacity: 1,
+			}}>
+			<Container maxWidth='lg' className={styles.container}>
+				{children}
+			</Container>
+		</div>
 	);
 };
