713ac6c99ec4fe26c43909aaac01636493656075
Franklin Von Moon <42459707+OperationFman@users.noreply.github.com>
2025-09-04 14:16:46 +0700

    :fire: Fix Head import for _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from "next/app";
 import "prismjs/themes/prism-tomorrow.css";
 import { Analytics } from "@vercel/analytics/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "react-notion-x/src/styles.css";
 import { Navbar } from "../src/global/navigation/Navbar";
 import "../themes/globals.css";
 import { GlobalTheme } from "../themes/GlobalTheme";
-import { Head } from "next/document";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const [darkMode, setDarkMode] = useState(true);
